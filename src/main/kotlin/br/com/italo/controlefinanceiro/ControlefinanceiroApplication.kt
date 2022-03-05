89fa91f6f5540bcda8e1ba21a8cbd3c2fa447dab
dxt.icoimbra <dxt.icoimbra@ifood3rd.com>
2022-03-05 14:57:29 -0300

    Criação do projeto

---
+++ src/main/kotlin/br/com/italo/controlefinanceiro/ControlefinanceiroApplication.kt
@@ -0,0 +1,11 @@
+package br.com.italo.controlefinanceiro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ControlefinanceiroApplication
+
+fun main(args: Array<String>) {
+	runApplication<ControlefinanceiroApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/italo/controlefinanceiro/ControlefinanceiroApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.italo.controlefinanceiro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ControlefinanceiroApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
